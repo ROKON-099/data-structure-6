856e585cc81ad80a929d13a9ae70100bd0e8948e
Rokon007 <mdrokonuzzaman099@gmail.com>
2026-03-10 20:53:52 +0600

    added a commit

---
+++ P34.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter size of array:";
+    cin>>n;
+    cout << "Enter " <<n << " number of array:";
+    char arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    int vowel=0, consonant=0;
+
+for (int i=0; i<n; i++)
+{
+
+
+    char ch=arr[i];
+    if (ch>='A' && ch<='Z')
+        ch=ch+32;
+    if (ch>='a' && ch<='z')
+    {
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+            vowel++;
+        else
+            consonant++;
+    }
+}
+    cout << "vowel number is :" <<vowel<<endl;
+    cout << " consonant number is: " <<consonant<<endl;
+
+
+
+
+}
+
---
+++ P35.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter size of array:";
+    cin>>n;
+    cout << "Enter " <<n << " number of array:";
+    char arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    int vowel=0, conso=0;
+    for (int i=0; i<n; i++)
+    {
+        char ch=arr[i];
+
+        if (ch>='A' && ch<='Z')
+          ch=ch+32;
+
+          if (ch>='a' && ch<='z')
+          {
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+            vowel++;
+        else
+            conso++;
+    }
+
+    }
+    cout << "vowel:" <<vowel <<endl;
+    cout << "cons:" <<conso <<endl;
+
+}
+
